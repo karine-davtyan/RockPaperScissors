6661d4215fa12fc66d3439b9f4dd43cdadc432e0
karine-davtyan <96755306+karine-davtyan@users.noreply.github.com>
2024-09-16 16:41:21 -0400

    Game.java

--- Game.java
+++ Game.java
@@ -3,20 +3,19 @@ package rockpaperscissors;
 import java.util.Random;
 public class Game {
 	 private final String[] options = {"Rock", "Paper", "Scissors"};
-
+	
 	    public String getBotMove() {
 	        Random rand = new Random();
 	        return options[rand.nextInt(options.length)];
 	    }
-
 	    public String determineWinner(String userMove, String botMove) {
 	        if (userMove.equals(botMove)) {
 	            return "It's a draw!";
-	        } else if ((userMove.equals("Rock") && botMove.equals("Scissors")) ||
-	                   (userMove.equals("Paper") && botMove.equals("Rock")) ||
-	                   (userMove.equals("Scissors") && botMove.equals("Paper"))) {
+	        } 
+		else if ((userMove.equals("Rock") && botMove.equals("Scissors")) || (userMove.equals("Paper") && botMove.equals("Rock")) || (userMove.equals("Scissors") && botMove.equals("Paper"))) {
 	            return ("You win!");
-	        } else {
+	        } 
+		else {
 	            return ("Bot wins!");
 	        }
 	    }
